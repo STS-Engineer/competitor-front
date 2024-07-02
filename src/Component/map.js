59e9f40eaf25bc011d621c1825ff1d5a96337591
STS-Engineer <administration.sts@avocarbon.com>
2024-07-02 09:09:49 +0100

    Update map.js

--- src/Component/map.js
+++ src/Component/map.js
@@ -86,7 +86,7 @@ function Map() {
  
     const fetchCompanies = async () => {
         try {
-            const response = await axios.get('https://avo-adb-001.postgres.database.azure.com:5432/companies');
+            const response = await axios.get('https://avo-competitor-map-backend.azurewebsites.net/companies');
             setCompanies(response.data);
  
             // Extract company names from the fetched data
